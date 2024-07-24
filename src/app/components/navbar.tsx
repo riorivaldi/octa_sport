684adc3418897f8516cf1d56f87cd247ae7fcf1d
RioRivaldi <rivaldirio090@gmail.com>
2024-07-24 16:49:54 +0700

    update icon dan image

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -3,19 +3,20 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="bg-white p-6 flex flex-col">
-        <h1 className="mb-2 text-black cursor-pointer hover:underline text-xs mx-auto">
+    <div className="fixed w-full">
+      <div className="bg-opacity-90 p-6 flex flex-col w-full bg-white ">
+       
+
+        <h1 className="mb-2 text-black  cursor-pointer text-xs mx-auto">
           GET SHIPPING DISCOUNT OF IDR 9.000 WITH MINIMUM PURCHASE OF IDR
           500.000
         </h1>
+       
         <p className="border-b-2 border-black"></p>
-
         <div>
           <div>
-            <nav className="fixed bg-white bg-opacity-30 w-full">
-              <div className="grid grid-cols-2"></div>
-              <div className="mx-auto flex items-center">
+            <nav className="fixed bg-white bg-opacity-95 w-full">
+              <div className=" flex items-center justify-between">
                 <div className="flex items-center space-x-1">
                   <Link href="#home">
                     <p className="flex px-5">
@@ -26,27 +27,55 @@ const Navbar = () => {
                         height={70}
                       />
                     </p>
-                    <div className="mb-5 font-serif text-[8px] mx-6">
+                    <div className="mb-5 font-serif text-[8px] mx-7">
                       OCTA SPORT
                     </div>
                   </Link>
+                  <div className="flex grid-cols-2 mb-5 items-center justify-between space-x-4 top-10">
+                    <Link href="#">
+                      <p className="text-lg font-bold text-black hover:text-blue-900 ">
+                        HOME
+                      </p>
+                    </Link>
+                    <Link href="#FOOTBAL">
+                      <p className="text-lg font-bold text-black hover:text-blue-900">
+                        FOOTBAL
+                      </p>
+                    </Link>
+                    <Link href="#FUTSAL">
+                      <p className="text-lg font-bold text-black hover:text-blue-900">
+                        FUTSAL
+                      </p>
+                    </Link>
+                    <Link href="#RUNNING">
+                      <p className="text-lg font-bold text-black hover:text-blue-900">
+                        RUNNING
+                      </p>
+                    </Link>
+                    <Link href="#ACCESSORIES">
+                      <p className="text-lg font-bold text-black hover:text-blue-900">
+                        ACCESSORIES
+                      </p>
+                    </Link>
+                  </div>
                 </div>
-                <div className="flex grid-cols-2 mb-5 items-center space-x-4 me-8 top-10">
-                  <Link href="#">
-                    <p className="flex  text-black font-bold ">HOME</p>
-                  </Link>
-                  <Link href="#FOOTBAL">
-                    <p className="text-black font-bold">FOOTBAL</p>
-                  </Link>
-                  <Link href="#FUTSAL">
-                    <p className="text-black font-bold">FUTSAL</p>
-                  </Link>
-                  <Link href="#RUNNING">
-                    <p className="text-black font-bold">RUNNING</p>
-                  </Link>
-                  <Link href="#ACCESSORIES">
-                    <p className="text-black font-bold">ACCESSORIES</p>
-                  </Link>
+
+                <div className="mr-10 flex space-x-2">
+                  <Image
+                    src="/img/Search.svg"
+                    alt="Logo"
+                    width={50}
+                    height={50}
+                  />
+
+                  <Image
+                    src="/img/User.svg"
+                    alt="Logo"
+                    width={45}
+                    height={45}
+                  />
+
+                  <Image src="/img/1.svg" alt="Logo" width={50} height={50} />
                 </div>
               </div>
             </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import Navbar from './components/navbar'
+import Image from 'next/image'
 
 
 export default function page() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div className="grid grid-cols-2 items-center min-h-screen w-full ">
+        <div className='ml-7 mt-14'>
+          <Image
+            src="/img/Rectangle 1.png"
+            alt="icon"
+            width={600}
+            height={400}
+            className="h-auto"
+          />
+        </div>
+        <div className="ml-7 mt-14">
+          <Image
+            src="/img/Rectangle 2.png"
+            alt="icon"
+            width={600}
+            height={400}
+            className="h-auto"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
